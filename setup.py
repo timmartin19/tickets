bbe4b996901bd1e4aec1e4402ee31a64d30a45e0
timmartin19 <martin@tims-mbp.fios-router.home>
2014-11-27 14:14:27 -0500

    More setup

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
 __author__ = 'Tim Martin'
+from setuptools import setup
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'tickets', 'VERSION')) as f:
+    version = f.read().strip()
+
+setup(
+    name='martin-tickets',
+    version=version,
+    include_package_data=True,
+    packages=['tickets', 'ticketsapp'],
+    license='Private',
+    author='Tim Martin',
+    author_email='tim.martin@vertical-knowledge.com',
+    description='A web app for managing tickets submitted and completed',
+    install_requires=['django', 'djangorestframework', 'django-filter']
+)
--- tickets/settings.py
+++ tickets/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'ticketsapp',
+    'djangobower',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +84,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'djangobower.finders.BowerFinder',
+)
+
+BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, 'bower_components')
+
+BOWER_PATH = '/usr/local/bin/bower'
+
+BOWER_INSTALLED_APPS = (
+    'ember',
+    'ember-data#1.0.0-beta.12',
+)
