f2b2106aec7af0ac25d11d00afdb943dff7e750e
timmartin19 <martin@tims-mbp.fios-router.home>
2014-11-27 14:51:33 -0500

    Added ticket model

--- ticketsapp/models.py
+++ ticketsapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Ticket(models.Model):
+    client = models.ForeignKey(User, related_name='client_tickets')
+    consultant = models.ForeignKey(User, related_name='consultant_tickets')
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    due_date = models.DateTimeField()
+    date_submitted = models.DateTimeField(auto_created=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=12, blank=True, null=True)
+    progress_report = models.TextField(blank=True, null=True)
+    finished = models.BooleanField(default=False)
