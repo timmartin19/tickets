4ba5ff27a9a4ab90c975382bab9d797c1141bbb1
timmartin19 <tim.martin@vertical-knowledge.com>
2014-11-30 17:05:27 -0500

    deployment shit

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,5 @@
 __author__ = 'Tim Martin'
-from fabric.api import run, local, env, cd, settings, put, hide, get, lcd
+from fabric.api import run, local, env, cd, settings, put, hide, get, lcd, prefix
 from fabvenv import virtualenv
 import fabfile_deets
 import xmlrpclib
@@ -13,11 +13,13 @@ server_ip = fabfile_deets.server_ip
 
 webfaction_account_name = env.user
 project_name = 'tickets'
+app_name = 'ticketsapp'
 domain = 'martin-consulting.org'
 webapp_path = '/home/%s/webapps/%s_app/%s_app' % (webfaction_account_name, project_name, project_name)
 db_password = '90mjQgWhZn'
 virtual_env_wrapper = '~/bin/virtualenvwrapper.sh'
-venv_name = '{0}_venv'.format(project_name)
+venv_name = '{0}'.format(project_name)
+static_path = '/home/{0}/webapps/{1}_app/{1}_app/static_collected'.format(webfaction_account_name, project_name, app_name)
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -35,6 +37,24 @@ def get_logs():
     get('/home/{0}/logs/user/access_{1}_app.log*'.format(env.user, project_name))
     get('/home/{0}/logs/user/error_{1}_app.log*'.format(env.user, project_name))
 
+@run_in_local_dir
+def get_conf():
+    get('/home/{0}/webapps/{1}_app/apache2/conf/httpd.conf'.format(webfaction_account_name, project_name))
+
+@run_in_local_dir
+def deploy_conf():
+    put(os.path.join(base_dir, 'deployment', 'httpd.conf'),
+        '/home/{0}/webapps/{1}_app/apache2/conf'.format(webfaction_account_name, project_name))
+    put(os.path.join(base_dir, 'deployment', 'wsgi.py'),
+        '/home/{0}/webapps/{1}_app/{1}_app/{2}'.format(webfaction_account_name, project_name, app_name))
+
+@run_in_local_dir
+def deploy_static():
+    local('python manage.py bower install')
+    local('python manage.py collectstatic')
+    put(os.path.join(base_dir, 'static_collected'),
+        '/home/{0}/webapps/{1}_app/{1}_app'.format(webfaction_account_name, project_name))
+
 
 def pack():
     local('git archive --format zip master --output=%s.zip' % project_name, capture=False)
@@ -49,22 +69,22 @@ def create_virtual_environment():
 def deploy():
     # local("pip freeze > requirements.txt -f 'http://vkpypi:8EWash!@pypi.vkspider.com/packages'")
     pack()
-    put('%s.zip' % project_name, '/home/%s/' % webfaction_account_name)
-    with settings(hide('warnings', 'running', 'stdout', 'stderr'), warn_only=True):
-        run('unzip -o /home/%s/%s.zip -d %s' % (webfaction_account_name, project_name, webapp_path))
-
+    put('{0}.zip'.format(project_name), '/home/{0}/'.format(webfaction_account_name))
+    run('unzip -o /home/{0}/{1}.zip -d {2}'.format(webfaction_account_name, project_name, webapp_path))
     with cd(webapp_path):
-        print(webapp_path)
-        with virtualenv(venv_name):
+        with prefix('workon {0}'.format(venv_name)):
             run('pip install -r requirements.txt')
+    deploy_conf()
     deploy_settings()
+    deploy_static()
     restart_apache()
-    run('rm /home/%s/%s.zip' % (webfaction_account_name, project_name))
+    run('rm /home/{0}/{1}.zip'.format(webfaction_account_name, project_name))
+    local('rm {0}.zip'.format(project_name))
 
 @run_in_local_dir
 def deploy_settings():
-    put('settings.py', os.path.join(webapp_path, 'oauthapp'))
-    put('index.py', '/home/vkstage/webapps/{0}_app/htdocs'.format(project_name))
+    put('{0}/production.py'.format(project_name),
+        '/home/{1}/webapps/{0}_app/{0}_app/{0}'.format(project_name, webfaction_account_name))
 
 
 def restart_apache():
@@ -88,9 +108,6 @@ def create_webfaction_site():
     print "creating app..."
     server.create_app(session_id, "%s_app" % project_name, 'mod_wsgi34-python27', False, '')
     print "creating media app..."
-    static_path = '/home/%s/webapps/%s_app/%s_app/oauthapp/static/' % (
-        webfaction_account_name, project_name, project_name)
-    print static_path
     server.create_app(session_id, "%s_static" % project_name, 'symlink', False, static_path)
     print "creating website..."
     server.create_website(session_id, "%s_website" % project_name, server_ip, False, [domain],
---
+++ tickets/development.py
@@ -0,0 +1,2 @@
+__author__ = 'Tim Martin'
+from settings import *
